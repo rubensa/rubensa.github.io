937c9f694392817a2d7f8d6dae0d517a74b32c04
Ruben Suarez Alvarez <rubensa@gmail.com>
2018-06-13 20:57:15 +0200

    Initial commit

---
+++ js/library.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+  init_document_ready();
+
+});
+
+// - - - - - - - - - - - - - - - - - - - - - - - - -
+/* - - - - - - - - - - - - - - - - - - - - - - - - -
+ * Anything that should be run as soon as DOM is
+ * loaded.
+ * - - - - - - - - - - - - - - - - - - - - - - - - -
+ */
+
+function init_document_ready() {
+
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
